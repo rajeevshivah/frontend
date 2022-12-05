2ec3dc888bd31c0ffe128e63a9b5a1e44690819b
raajeev.ranjann <raajeev.ranjann@gmail.com>
2022-12-05 21:27:14 +0530

    Asome updates

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <div>Users</div>;
+  const USERS = [
+    {
+      id: 1,
+      name: "John",
+      image:
+        "https://i.pinimg.com/originals/96/5d/30/965d309aa4e3c8f25966d0069843f1db.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
