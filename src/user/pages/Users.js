ab67c419662a45c6b49da7c2cabc93048d8df0ac
rajeevshivah <raajeev.ranjann@gmail.com>
2023-01-14 19:34:59 +0530

    Modal added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,14 +4,14 @@ import UsersList from "../components/UsersList";
 const Users = () => {
   const USERS = [
     {
-      id: 1,
+      id: 'u1',
       name: "John",
       image:
         "https://i.pinimg.com/originals/96/5d/30/965d309aa4e3c8f25966d0069843f1db.jpg",
       places: 3,
     },
     {
-      id: 2,
+      id: 'u2',
       name: "Anuj",
       image:
         "https://i.pinimg.com/originals/96/5d/30/965d309aa4e3c8f25966d0069843f1db.jpg",
